1a5989069c23de4bb3881233ebb3164bdb539fbf
Omar Ahmed <omarking05@gmail.com>
2021-12-30 00:55:45 +0200

    feat(nvim): add which key plugin

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -113,6 +113,17 @@ hooks.add("install_plugins", function(use)
     end,
   }
 
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
   -- use {"sbdchd/neoformat"}
 
   -- use {"github/copilot.vim"}
