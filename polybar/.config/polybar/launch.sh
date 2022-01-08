004f8336213dd7e2e79d7f6a380d0d2cb68abce6
Omar Ahmed <omarking05@gmail.com>
2022-01-08 14:19:04 +0200

    feat(polybar): use new polybar setup

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,8 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	  MONITOR=$m polybar --reload awesome &
+	  MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini main &
   done
 else
-	polybar --reload awesome &
+	polybar --reload awesome -c ~/.config/polybar/config.ini main &
 fi
