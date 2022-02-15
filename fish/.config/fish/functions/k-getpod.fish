9fd22a209135ff1a17f2bbfa8150ccaa681fa92f
Omar Ahmed <omarking05@gmail.com>
2022-02-15 18:02:52 +0200

    feat(fish): use kubectl instead of fish aliases

--- fish/.config/fish/functions/k-getpod.fish
+++ fish/.config/fish/functions/k-getpod.fish
@@ -1,3 +1,3 @@
 function k-getpod --description 'k-getpod <context> <resource_name>'
-  eval "k8dev$argv[1] get pod | grep -i $argv[2..-1] | cut -d ' ' -f 1 | head -n 1"
+  eval "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' -n default get pod | grep -i $argv[2..-1] | cut -d ' ' -f 1 | head -n 1"
 end
--- fish/.config/fish/functions/k-logs.fish
+++ fish/.config/fish/functions/k-logs.fish
@@ -1,8 +1,9 @@
 function k-logs --description 'k-logs <context> <resource_name>'
   set -l POD (k-getpod $argv[1] $argv[2])
-  set -l APP (eval "k8dev$argv[1] get pod $POD -o yaml | grep -i app: | cut -d ':' -f 2")
+  set -l BASE_COMMAND "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' -n default"
+  set -l APP (eval "$BASE_COMMAND get pod $POD -o yaml | grep -i app: | cut -d ':' -f 2")
   set -l APP (string trim $APP)
-  set -l COMMAND "k8dev$argv[1] logs --tail=-1 -l app=$APP $argv[3..-1]"
+  set -l COMMAND "$BASE_COMMAND logs --tail=-1 -l app=$APP $argv[3..-1]"
 
   if test $status -ne 0
     echo "Something went wrong, status is not 0. Ignoring..."
