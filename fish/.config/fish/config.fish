5afbba174949305b757f19aa5517b044c87cf60f
Omar Ahmed <omarking05@gmail.com>
2022-05-19 15:06:50 +0200

    fix(fish): remove fish ssh agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,9 @@ set -g -x fish_greeting ''
 set PATH /home/mrgeek/.local/bin $PATH
 set PATH /home/mrgeek/go/bin $PATH
 set PATH /home/mrgeek/.config/composer/vendor/bin $PATH
-set PATH /home/mrgeek/.fly/bin $PATH
-set PATH /home/mrgeek/.deta/bin $PATH
 set PATH /home/mrgeek/.cargo/bin $PATH
 set PATH /home/mrgeek/.gem/ruby/2.7.0/bin $PATH
-set PATH /home/mrgeek/.local/.npm-global/bin $PATH
+set PATH /home/mrgeek/n/bin $PATH
 
 source ~/.config/fish/private.fish
 
