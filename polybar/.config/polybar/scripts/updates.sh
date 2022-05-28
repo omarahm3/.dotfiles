30fa8b9543517f7205f3886aecc0ba710fa4cbce
Omar Ahmed <omarking05@gmail.com>
2022-05-28 21:57:02 +0200

    fix(polybar): supress stderr output

--- polybar/.config/polybar/scripts/updates.sh
+++ polybar/.config/polybar/scripts/updates.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-UPDATES=$(yay -Qqu | tr -s ' ' | wc -l)
+GET_UPDATES=$(yay -Qqu 2> /dev/null)
+
+if [ $? -ne 0 ]; then
+    echo " Error"
+    exit 1
+fi
+
+GET_UPDATES=$(echo $GET_UPDATES | tr -s ' ')
+
+if [ -z "$GET_UPDATES" ]; then
+  echo " "
+  exit 0
+fi
+
+UPDATES=$(echo $GET_UPDATES | wc -l)
 
 if [ $UPDATES -gt 0 ]; then
   echo " $UPDATES"
