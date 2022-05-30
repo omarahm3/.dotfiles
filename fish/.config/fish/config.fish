8df03c63e9abac82562ab92d0efc73e6fc2ff7b6
Omar Ahmed <omarking05@gmail.com>
2022-05-30 21:50:04 +0200

    feat(fish): add cattpuccin theme to FZF

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -57,3 +57,6 @@ if test -x (command -v fw)
 end
 
 alias cd z
+
+# FZF catppuccin theme
+set -Ux FZF_DEFAULT_OPTS "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
