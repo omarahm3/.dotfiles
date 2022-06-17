2d831ef4f30f3743cd4b2a97806e01a102ffa515
Omar Ahmed <omarking05@gmail.com>
2022-06-17 15:42:51 +0200

    feat(getpod): use jq to filter out pods

--- fish/.config/fish/functions/k-getpod.fish
+++ fish/.config/fish/functions/k-getpod.fish
@@ -1,3 +1,12 @@
 function k-getpod --description 'k-getpod <context> <resource_name>'
-  eval "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' get pod --all-namespaces | grep -i $argv[2..-1] | tr -s ' ' | cut -d ' ' -f 2 | head -n 1"
+  set -l KUBE_OUTPUT (kubectl --kubeconfig="/home/mrgeek/.kube/$argv[1].yaml" get pod -n $argv[1] -o json 2>&1)
+  set -l IS_EMPTY (echo $KUBE_OUTPUT | jq -r '.items[]' | string trim)
+
+  if test -z "$IS_EMPTY"
+    set KUBE_OUTPUT (kubectl --kubeconfig="/home/mrgeek/.kube/$argv[1].yaml" get pod --all-namespaces -o json 2>&1)
+  end
+
+  set -l COMMAND_STRING (echo $KUBE_OUTPUT | jq -r "[.items[] | select(.metadata.name | contains(\"$argv[2..-1]\"))][0]")
+  set -l OUTPUT (echo $COMMAND_STRING | jq -r '.metadata.name')
+  echo $OUTPUT
 end
