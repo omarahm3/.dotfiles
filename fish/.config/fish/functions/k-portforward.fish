1c33d5a08639c0bddd30554cb19ae039534455f1
Omar Ahmed <omarking05@gmail.com>
2022-06-21 10:43:00 +0200

    feat(fish): add namespace to k8s commands

--- fish/.config/fish/functions/k-portforward.fish
+++ fish/.config/fish/functions/k-portforward.fish
@@ -1,6 +1,6 @@
 function k-portforward --description 'k-portforward <context> <name> <ports>'
   set -l POD (k-getpod $argv[1] $argv[2])
-  set -l COMMAND "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' port-forward $POD $argv[3]"
+  set -l COMMAND "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' port-forward $POD $argv[3] -n $argv[1]"
 
   eval $COMMAND
 
--- fish/.config/fish/functions/k-ssh.fish
+++ fish/.config/fish/functions/k-ssh.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function k-ssh
-  eval "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' exec -it $argv[2] -- bash"
+  eval "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' -n $argv[1] exec -it $argv[2] -- bash"
 end
