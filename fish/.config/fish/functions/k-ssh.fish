db261024a1871fb84b02828064aae493ce666045
Omar Ahmed <omarking05@gmail.com>
2022-07-06 14:09:10 +0200

    feat(k-ssh): handle new infra changes

--- fish/.config/fish/functions/k-ssh.fish
+++ fish/.config/fish/functions/k-ssh.fish
@@ -1,4 +1,13 @@
 # Defined in - @ line 1
 function k-ssh
-  eval "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' -n $argv[1] exec -it $argv[2] -- bash"
+  if begin string match -q "dev*" "$argv[1]"; and string match -q "dev-eks" "$CURRENT_KUBE_CLUSTER"; end
+    set COMMAND "kubectl -n $argv[1]"
+  else if begin string match -q "dev*" "$argv[1]"; and not string match -q "dev-eks" "$CURRENT_KUBE_CLUSTER"; end
+    echo "Current cluster is not right [$CURRENT_KUBE_CLUSTER]"
+    return 1
+  else
+    set COMMAND "kubectl"
+  end
+
+  eval "$COMMAND exec -it $argv[2] -- bash"
 end
