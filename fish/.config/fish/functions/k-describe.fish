2e22798e9603603503296a0b969f00716b3a0cfc
Omar Ahmed <omarking05@gmail.com>
2022-07-06 13:59:36 +0200

    feat(k-describe): remove kubeconfig flag

--- fish/.config/fish/functions/k-describe.fish
+++ fish/.config/fish/functions/k-describe.fish
@@ -1,3 +1,3 @@
 function k-describe --description 'k-getpod <context> <resource_type> <resource_name>'
-  eval "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' -n default describe $argv[2] $argv[3]"
+  eval "kubectl -n $argv[1] describe $argv[2]/$argv[3]"
 end
