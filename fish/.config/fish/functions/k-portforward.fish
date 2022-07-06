98edd6a73b08246ec5890f4724e6e1f35edbe22d
Omar Ahmed <omarking05@gmail.com>
2022-07-06 13:57:47 +0200

    feat(k-portforward): handle new infra and add more checks

--- fish/.config/fish/functions/k-portforward.fish
+++ fish/.config/fish/functions/k-portforward.fish
@@ -1,13 +1,18 @@
 function k-portforward --description 'k-portforward <context> <name> <ports>'
   set -l POD (k-getpod $argv[1] $argv[2])
-  set -l COMMAND "kubectl --kubeconfig='/home/mrgeek/.kube/$argv[1].yaml' port-forward $POD $argv[3]"
+  
+  if string match -q "null" "$POD"
+    echo "Pod was not found"
+    return 1
+  end
 
-  # Add namespace if trying to access some clusters
-  switch $argv[1]
-    case 'dev*'
-      set COMMAND "$COMMAND -n $argv[1]"
-    case '*'
-      set COMMAND "$COMMAND -n port-forward"
+  if begin string match -q "dev*" "$argv[1]"; and string match -q "dev-eks" "$CURRENT_KUBE_CLUSTER"; end
+    set COMMAND "kubectl -n $argv[1] port-forward $POD $argv[3]"
+  else if begin string match -q "dev*" "$argv[1]"; and not string match -q "dev-eks" "$CURRENT_KUBE_CLUSTER"; end
+    echo "Current cluster is not right [$CURRENT_KUBE_CLUSTER]"
+    return 1
+  else
+    set COMMAND "kubectl -n port-forward port-forward $POD $argv[3]"
   end
 
   eval $COMMAND
