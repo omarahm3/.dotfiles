64a6b507ede1c9e1b3e18c68cb93f10a24c6c4a2
Omar Ahmed <me@mrg.sh>
2023-01-13 14:04:08 +0200

    feat(fish): customize docker run function

--- fish/.config/fish/functions/d-runit.fish
+++ fish/.config/fish/functions/d-runit.fish
@@ -1,3 +1,16 @@
-function d-runit --description 'd-runit <image_name_or_hash>'
- docker run -it $argv[1] /bin/bash 
+function d-runit --description 'd-runit <image_name_or_hash> <shell> <user>'
+  set ISHELL (echo $argv[2])
+  set IUSER (echo $argv[3])
+
+  if test -z "$ISHELL"
+    set ISHELL "/bin/bash"
+    return 1
+  end
+
+  if test -z "$IUSER"
+    set IUSER "root"
+    return 1
+  end
+
+  docker run -it -u $IUSER $argv[1] $ISHELL
 end
