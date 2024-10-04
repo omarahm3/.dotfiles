6ed9fe07e5d4653e7e0353b5c12e341bd95d6fa9
Omar Ahmed <me@mrg.sh>
2024-10-04 12:44:44 +0200

    feat(lvim): enable go debug and setup text folding yamlls

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -312,7 +312,7 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.treesitter.indent.disable = { "python" }
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright", "yamlls" })
 local opts = {
   cmd = { "pyright-langserver", "--stdio", "--generate-members" },
   filetypes = { "python" },
@@ -325,6 +325,17 @@ local opts = {
   },
 }
 require("lvim.lsp.manager").setup("pyright", opts)
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
+}
+
+require("lvim.lsp.manager").setup("yamlls", {
+  capabilities = capabilities,
+})
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
@@ -625,3 +636,75 @@ lvim.plugins = {
     },
   }
 }
+
+lvim.builtin.dap.active = true
+local dap = require("dap")
+
+dap.adapters.go = function(callback, _)
+  local stdout = vim.loop.new_pipe(false)
+  local handle
+  local pid_or_err
+  local port = 38697
+  local opts = {
+    stdio = { nil, stdout },
+    args = { "dap", "-l", "127.0.0.1:" .. port },
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+    stdout:close()
+    handle:close()
+    if code ~= 0 then
+      print("dlv exited with code", code)
+    end
+  end)
+  assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+  -- Wait for delve to start
+  vim.defer_fn(function()
+    callback { type = "server", host = "127.0.0.1", port = port }
+  end, 100)
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  {
+    type = "go",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}",
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "go",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}",
+  },
+}
+
+local function GoPrettyTag()
+  local filename = vim.fn.expand('%')
+  local command = 'formattag -file ' .. vim.fn.shellescape(filename)
+  vim.fn.system(command)
+  vim.cmd('silent! write!')
+end
+
+vim.api.nvim_create_user_command('PrettyTag', GoPrettyTag, { nargs = 0 })
+vim.api.nvim_create_autocmd({ "BufWritePost", "FileWritePost" }, {
+  pattern = "*.go",
+  callback = GoPrettyTag,
+})
