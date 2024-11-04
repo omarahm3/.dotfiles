1ce966e989369260d55206ead5de76fc2e9974e6
Omar Ahmed <me@mrg.sh>
2024-11-04 09:46:45 +0100

    feat: update to latest

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -565,76 +565,93 @@ lvim.plugins = {
     }
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
+    -- Make sure to setup it properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
     opts = {
-      provider = "ollama",
-      vendors = {
-        ollama = {
-          ["local"] = true,
-          endpoint = "localhost:11434/v1",
-          model = "deepseek-coder:33b",
-          parse_curl_args = function(opts, code_opts)
-            return {
-              url = opts.endpoint .. "/chat/completions",
-              headers = {
-                ["Accept"] = "application/json",
-                ["Content-Type"] = "application/json",
-              },
-              body = {
-                model = opts.model,
-                messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-                max_tokens = 2048,
-                stream = true,
-              },
-            }
-          end,
-          parse_response_data = function(data_stream, event_state, opts)
-            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-          end,
-        },
-      }
+      file_types = { "markdown", "Avante" },
     },
-    keys = {
-      { "<leader>aa", function() require("avante.api").ask() end,     desc = "avante: ask",    mode = { "n", "v" } },
-      { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
-      { "<leader>ae", function() require("avante.api").edit() end,    desc = "avante: edit",   mode = "v" },
-    },
+    ft = { "markdown", "Avante" },
+  },
+  {
+    'linux-cultist/venv-selector.nvim',
     dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to setup it properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-  }
+    opts = {
+      name = ".venv",
+      auto_refresh = true,
+    },
+    lazy = false,
+    branch = "regexp",
+    keys = {
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+    },
+  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   opts = {
+  --     provider = "ollama",
+  --     vendors = {
+  --       ollama = {
+  --         ["local"] = true,
+  --         endpoint = "localhost:11434/v1",
+  --         model = "deepseek-coder:33b",
+  --         parse_curl_args = function(opts, code_opts)
+  --           return {
+  --             url = opts.endpoint .. "/chat/completions",
+  --             headers = {
+  --               ["Accept"] = "application/json",
+  --               ["Content-Type"] = "application/json",
+  --             },
+  --             body = {
+  --               model = opts.model,
+  --               messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+  --               max_tokens = 2048,
+  --               stream = true,
+  --             },
+  --           }
+  --         end,
+  --         parse_response_data = function(data_stream, event_state, opts)
+  --           require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+  --         end,
+  --       },
+  --     }
+  --   },
+  --   keys = {
+  --     { "<leader>aa", function() require("avante.api").ask() end,     desc = "avante: ask",    mode = { "n", "v" } },
+  --     { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
+  --     { "<leader>ae", function() require("avante.api").edit() end,    desc = "avante: edit",   mode = "v" },
+  --   },
+  --   dependencies = {
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua",      -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- }
 }
 
 lvim.builtin.dap.active = true
