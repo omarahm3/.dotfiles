7c43f958ffffa73169d5990a3580a2424ebeb88e
Omar Ahmed <me@mrg.sh>
2024-11-06 12:26:22 +0100

    feat(nvim): configure codeium keymaps

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -25,10 +25,17 @@ return {
     "Exafunction/codeium.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      -- "hrsh7th/nvim-cmp",
     },
     config = function()
       require("codeium").setup({
+        virtual_text = {
+          enabled = true,
+          key_bindings = {
+            accept = "<C-g>",
+          },
+        },
       })
-    end
+    end,
   },
 }
