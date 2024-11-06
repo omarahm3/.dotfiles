69b137e353b9deaed2e5f9fb9140c9de84590654
Omar Ahmed <me@mrg.sh>
2024-11-06 11:32:15 +0100

    feat(nvim): install needed plugins

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+		end,
+	},
+}
---
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,34 @@
+return {
+  -- {
+  --   'Exafunction/codeium.nvim',
+  --   event = 'BufEnter',
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   config = function()
+  --     vim.g.coeium_no_map_tab = 1
+  --     vim.g.codeium_disable_bindings = 1
+
+  --     vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+  --     vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+  --     vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+  --     vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  --   end,
+  --   keys = {
+  --     { "<C-g>", function() return vim.fn['codeium#Accept']() end,             mode = "i", desc = "Codeium Accept" },
+  --     { "<c-;>", function() return vim.fn['codeium#CycleCompletions'](1) end,  mode = "i", desc = "Codeium Cycle Completions" },
+  --     { "<c-,>", function() return vim.fn['codeium#CycleCompletions'](-1) end, mode = "i", desc = "Codeium Cycle Completions" },
+  --     { "<c-x>", function() return vim.fn['codeium#Clear']() end,              mode = "i", desc = "Codeium Clear" },
+  --   },
+  -- },
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("codeium").setup({
+      })
+    end
+  },
+}
