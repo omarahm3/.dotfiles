da525c72528e0f738a5bf41d633d590123db100b
Omar Ahmed <me@mrg.sh>
2025-03-05 14:05:02 +0100

    feat(gopls): add integration tests to build

---
+++ nvim/.config/nvim/lua/plugins/gopls.lua
@@ -0,0 +1,22 @@
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    config = {
+      -- the key is the server name to configure
+      -- the value is the configuration table
+      gopls = {
+        settings = {
+          gopls = {
+            buildFlags = { "-tags=integration" },
+            analyses = {
+              shadow = false,
+              -- fieldalignment = true,
+            },
+            gofumpt = false,
+          },
+        },
+      },
+    },
+  },
+}
