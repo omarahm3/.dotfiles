926f2c4a4494423ab277a06e1e98ce72c89e476d
Omar Ahmed <me@mrg.sh>
2025-08-11 02:05:14 +0200

    feat(nvim): add supermaven

---
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,12 @@
+return {
+	"supermaven-inc/supermaven-nvim",
+	config = function()
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<C-g>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-j>",
+			},
+		})
+	end,
+}
