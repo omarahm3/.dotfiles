c950ced8029f3d670fbd80b1ace6b798aa338b60
Omar Ahmed <me@mrg.sh>
2025-08-14 13:34:01 +0200

    feat: latest updates

--- fish/.config/fish/functions/ec2.fish
+++ fish/.config/fish/functions/ec2.fish
@@ -12,7 +12,26 @@ function ec2 --description 'ec2 <key> [user@]<name> [<..rest>]'
         set name $user_and_name
     end
 
-    set -l EC2_IP (aws ec2 describe-instances --filters "Name=tag:Name,Values=*$name*" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].PublicIpAddress" --output json | jq -r '.[]')
+    # Get only running instances with the specified name pattern
+    set -l EC2_IP (aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=*$name*" \
+        "Name=instance-state-name,Values=running" \
+        --query "Reservations[].Instances[].PublicIpAddress" \
+        --output json | jq -r '.[]' | head -n1)
     
+    # Check if we got a valid IP
+    if test -z "$EC2_IP" -o "$EC2_IP" = "null"
+        echo "Error: No running EC2 instance found with name pattern '*$name*'"
+        return 1
+    end
+    
+    # Check if we got multiple IPs (shouldn't happen with head -n1, but safety check)
+    set -l ip_count (echo "$EC2_IP" | wc -l)
+    if test $ip_count -gt 1
+        echo "Error: Multiple running instances found with name pattern '*$name*'. Please be more specific."
+        return 1
+    end
+    
+    echo "Connecting to $user@$EC2_IP"
     ssh -i $key $user@$EC2_IP $rest_args
 end
