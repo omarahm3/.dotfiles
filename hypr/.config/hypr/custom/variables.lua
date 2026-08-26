60dc8b481026998fe3ecdadaa2d8a8b7f2082d9d
Omar Ahmed <me@mrg.sh>
2026-08-27 00:41:02 +0200

    feat(hypr): migrate config from hyprlang to lua

---
+++ hypr/.config/hypr/custom/variables.lua
@@ -0,0 +1,4 @@
+-- Upstream tries foot before kitty. Prefer kitty.
+terminal = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'kitty -1' 'foot' 'alacritty' 'wezterm'"
+
+fileManager = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'nautilus --new-window' 'dolphin' 'nemo' 'thunar'"
